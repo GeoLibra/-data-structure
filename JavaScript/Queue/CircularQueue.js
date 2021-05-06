7a918dc10d8b2809c1e5c490eeddfcb0bf040eff
GeoLibra <674530915@qq.com>
2021-05-06 21:40:32 +0800

    Create CircularQueue.js

---
+++ JavaScript/Queue/CircularQueue.js
@@ -0,0 +1,51 @@
+/**
+ * 循环队列
+ *
+ * @class CircularQueue
+ */
+class CircularQueue {
+  constructor(k) {
+    this.queue = [];
+    this.front = null;
+    this.rear = null;
+    // 队列长度
+    this.count = k;
+  }
+
+  enqueue(value) {
+    if (this.isFull()) {
+      return false;
+    } else {
+      this.queue[this.rear] = value;
+      // 尾指针循环
+      this.rear = (this.rear + 1) % this.count;
+    }
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return false;
+    } else {
+      const value = this.queue[this.front];
+      this.front = (this.front + 1) % this.count;
+      return value;
+    }
+  }
+
+  getFront() {
+    if(this.isEmpty()){
+      return false;
+    }else{
+      let rear = this.rear - 1
+      return this.queue[rear < 0 ? this.count - 1 : rear]
+    }
+  }
+
+  isFull() {
+    return this.front === this.rear && !!this.queue[this.front];
+  }
+
+  isEmpty() {
+    return this.front === this.rear && !this.queue[this.front];
+  }
+}
